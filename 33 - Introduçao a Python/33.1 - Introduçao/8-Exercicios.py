5d12d749ce2d4da7c3ee26aeaf915efe3205b1b7
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-15 17:56:49 -0300

    bonus - brincando com os asteriscos

--- 33 - Introduçao a Python/33.1 - Introduçao/8-Exercicios.py
+++ 33 - Introduçao a Python/33.1 - Introduçao/8-Exercicios.py
@@ -13,15 +13,33 @@ def maior(num1,num2):
 
 print("maior entre um e tres:", maior(1,3))
 print("maior entre nove e tres:",maior(9,3))
+print("--------------------------")
 
 def avg(data):
   return sum(data)/len(data)
 
 print("média entre os itens de [1,2]:", avg([1,2]))
 print("média entre os itens de [1,2,3]:", avg([1,2,3]))
+print("--------------------------")
 
 def printAst(num): 
   for i in range(num):
        print("*" * num)
 
 printAst(6)
+
+print("--------------------------")
+def printVertRetAst(num): 
+  for i in range(num):
+      for j in range(num):
+       print("*" * num)
+
+printVertRetAst(2)
+
+print("--------------------------")
+
+def printHorRetAst(num): 
+  for i in range(num):
+       print("*" * 2 * num)
+
+printHorRetAst(2)
