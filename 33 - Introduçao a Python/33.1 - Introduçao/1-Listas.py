92776d3c01871b1e0ab1009de896a82ec756f8ae
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-15 18:01:24 -0300

    33.2 - entrada e saida - inicio

--- 33 - Introduçao a Python/33.1 - Introduçao/1-Listas.py
+++ 33 - Introduçao a Python/33.1 - Introduçao/1-Listas.py
@@ -11,5 +11,5 @@ fruits.remove("pineapple")  # removendo uma fruta
 fruits.extend(["pear", "melon", "kiwi"])  # acrescenta uma lista de frutas a lista original
 
 fruits.index("apple")  # retorna o índice onde a fruta está localizada, neste caso 1
- em seu programa
+
 fruits.sort()  # ordena a lista de frutas
