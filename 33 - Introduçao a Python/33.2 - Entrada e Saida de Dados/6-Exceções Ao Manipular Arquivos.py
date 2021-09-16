6685661af6a481af2d73c6638f14f6c17169d83f
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-16 18:06:22 -0300

    exceçoes

---
+++ 33 - Introduçao a Python/33.2 - Entrada e Saida de Dados/6-Exceções Ao Manipular Arquivos.py
@@ -0,0 +1,28 @@
+try:
+    arquivo = open("inexistente.txt", "r")
+except OSError:
+    # será executado caso haja uma exceção
+    print("arquivo inexistente")
+else:
+    # será executado se tudo ocorrer bem no try
+    print("arquivo manipulado e fechado com sucesso")
+    arquivo.close()
+finally:
+    # será sempre executado, independentemente de erro
+    print("Tentativa de abrir arquivo")
+    print("__________________________")
+
+try:
+    arquivo = open("arquivo.txt", "r")
+except OSError:
+    # será executado caso haja uma exceção
+    print("arquivo inexistente")
+else:
+    # será executado se tudo ocorrer bem no try
+    print("arquivo manipulado e fechado com sucesso")
+    arquivo.close()
+finally:
+    # será sempre executado, independentemente de erro
+    print("Tentativa de abrir arquivo")
+    print("__________________________")
+  
