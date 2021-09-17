e2f20c5f095e6aa0a971363c6d6978699952a187
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-16 22:54:01 -0300

    jogo das palavras embaralhadas

--- 33 - Introduçao a Python/33.2 - Entrada e Saida de Dados/9- Exercicios.py
+++ 33 - Introduçao a Python/33.2 - Entrada e Saida de Dados/9- Exercicios.py
@@ -7,6 +7,25 @@ def invertScalPrint(name):
 
 invertScalPrint('adrian')
 
-scrambled_word = "string".join(random.sample("string", len("string")))
 
-print(scrambled_word)
+# Exercício 2: Jogo da palavra embaralhada. Desenvolva um jogo em que a
+# pessoa usuária tenha que adivinhar uma palavra que será mostrada com as
+# letras embaralhadas. O programa terá uma lista de palavras e escolherá
+# uma aleatoriamente. O jogador terá três tentativas para adivinhar a palavra.
+# Ao final a palavra deve ser mostrada na tela, informando se a pessoa
+# ganhou ou perdeu o jogo.
+
+def scrambledWordGame():
+  word = random.choice(["prismatic", "assiduous", "wonderful"]) 
+  scrambled_word = "".join(random.sample(word, len(word)))
+  attempts = 0
+  while (attempts < 3):
+   x = (input("Scrambled word is: %s, what word is it?   " %scrambled_word))
+   attempts += 1
+   if attempts == 3:
+    print("Correct answer is '%s'" %word) 
+   if x == word:
+     print('You Win!!')
+     break
+
+scrambledWordGame()
