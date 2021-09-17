c1ab3b299f3e9611e18149714ebb1ac4f75b836d
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-09-16 22:19:37 -0300

    ex1

--- 33 - Introduçao a Python/33.2 - Entrada e Saida de Dados/9- Exercicios.py
+++ 33 - Introduçao a Python/33.2 - Entrada e Saida de Dados/9- Exercicios.py
@@ -1,8 +1,12 @@
 # Exercício 1: Faça um programa que receba um 
 # nome e imprima o mesmo na vertical em escada invertida
-
+import random
 def invertScalPrint(name):
   for i in range(len(name)):
     print(name[slice(-i)])
 
 invertScalPrint('adrian')
+
+scrambled_word = "string".join(random.sample("string", len("string")))
+
+print(scrambled_word)
