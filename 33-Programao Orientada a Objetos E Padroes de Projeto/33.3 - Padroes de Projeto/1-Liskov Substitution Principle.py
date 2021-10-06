94a5c0d7acbf6f30f80a2581539a8056f02c7cfa
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-10-06 08:36:26 -0300

    inicio Padroes de Projeto

---
+++ 33-Programao Orientada a Objetos E Padroes de Projeto/33.3 - Padroes de Projeto/1-Liskov Substitution Principle.py
@@ -0,0 +1,30 @@
+from abc import ABS, abstractmethod
+
+class Connector(ABS):
+    @abstractmethod
+    def get_count(token):
+        pass
+
+    @abstractmethod
+    def count_request():
+        pass
+
+class RedisConnector(Connector):
+    def __init__(self, address, port):
+        # A lógica da conexão ao banco Redis
+
+    def get_count(token):
+        # A lógica de acesso ao banco Redis
+
+    def count_request(token):
+        # A lógica de acesso ao banco Redis
+
+class SqlConnector(Connector):
+    def __init__(self, address, port):
+        # A lógica da conexão ao banco SQL
+
+    def get_count(token):
+        # A lógica de acesso ao banco SQL
+
+    def count_request(token):
+        # A lógica de acesso ao banco SQL
